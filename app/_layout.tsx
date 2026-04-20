a5fa387bfe2a52ea346c80678eefcfb55c84c5fb
Sachintha Senanayake <sachintha.senanayake@brompton.co.uk>
2026-04-20 14:41:46 +0530

    create base _layout.tsx

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import colors from "../constants/colors";
+
+export default function RootLayout() {
+  // Save Area Provider: Ensures content won't bleed under the status bar / navigation buttons
+
+  return (
+    <SafeAreaProvider>
+      {/* Thin top-bar of the app; style='light' renders the icons in white */}
+      <StatusBar style="light" />
+
+      {/* Expo Router primitive to tell the screens to stack on top of each other */}
+      {/* headerShown: false; hides the default header bar globally */}
+      {/* contentStyle: applies a background color to every screen's content area */}
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.bgBase },
+        }}
+      >
+        {/* Register the (tabs) route in the stack */}
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </SafeAreaProvider>
+  );
+}
