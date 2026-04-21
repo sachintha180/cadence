dddf289a77f102b9123c18734cfe197234271e14
Sachintha Senanayake <sachintha.senanayake@brompton.co.uk>
2026-04-21 11:06:18 +0530

    update _layout.tsx; add ModelProvider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { ModelProvider } from "@/components/ModelProvider";
 import { ToastProvider } from "@/components/ToastProvider";
 import colors from "@/constants/colors";
 
@@ -11,23 +12,25 @@ export default function RootLayout() {
 
   return (
     <SafeAreaProvider>
-      <ToastProvider>
-        {/* Thin top-bar of the app; style='light' renders the icons in white */}
-        <StatusBar style="light" />
+      <ModelProvider>
+        <ToastProvider>
+          {/* Thin top-bar of the app; style='light' renders the icons in white */}
+          <StatusBar style="light" />
 
-        {/* Expo Router primitive to tell the screens to stack on top of each other */}
-        {/* headerShown: false; hides the default header bar globally */}
-        {/* contentStyle: applies a background color to every screen's content area */}
-        <Stack
-          screenOptions={{
-            headerShown: false,
-            contentStyle: { backgroundColor: colors.bgBase },
-          }}
-        >
-          {/* Register the (tabs) route in the stack */}
-          <Stack.Screen name="(tabs)" />
-        </Stack>
-      </ToastProvider>
+          {/* Expo Router primitive to tell the screens to stack on top of each other */}
+          {/* headerShown: false; hides the default header bar globally */}
+          {/* contentStyle: applies a background color to every screen's content area */}
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              contentStyle: { backgroundColor: colors.bgBase },
+            }}
+          >
+            {/* Register the (tabs) route in the stack */}
+            <Stack.Screen name="(tabs)" />
+          </Stack>
+        </ToastProvider>
+      </ModelProvider>
     </SafeAreaProvider>
   );
 }
