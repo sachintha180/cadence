161d5096e25b95d0908a38b0499e3d0be8b3ced3
Sachintha Senanayake <sachintha.senanayake@brompton.co.uk>
2026-04-21 11:06:40 +0530

    update recording.ts; add WAV2VEC_CHUNK_LENGTH

--- constants/recording.ts
+++ constants/recording.ts
@@ -3,3 +3,4 @@ export const MIN_VALID_RECORDING_MS = 10 * 1000;
 export const RECORDINGS_DIR_NAME = "recordings";
 export const PREPROCESSED_RECORDINGS_DIR_NAME = "preprocessed-recordings";
 export const WAV2VEC_SAMPLE_RATE = 16000;
+export const WAV2VEC_CHUNK_LENGTH = WAV2VEC_SAMPLE_RATE * 10;
